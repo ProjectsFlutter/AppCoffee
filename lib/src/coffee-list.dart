c57fa37c9750c60801761bac13d14f391847f385
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-08-31 16:00:02 -0500

    Coffe name design

--- lib/src/coffee-list.dart
+++ lib/src/coffee-list.dart
@@ -9,10 +9,13 @@ class CoffeeList extends StatefulWidget {
 class _CoffeeListState extends State<CoffeeList> {
   
   double _currentPage = 0.0;
+  double _textPage = 0.0;
 
   final _pageCoffeeController = PageController(
     viewportFraction: 0.35
   );
+
+  final _pageTextController = PageController();
 
   void _coffeScrollListener(){
     setState(() {
@@ -20,16 +23,23 @@ class _CoffeeListState extends State<CoffeeList> {
         });
   }
 
+  void _textScrollListener (){
+      _textPage = _currentPage;
+  }
+
   @override
     void initState() {
       _pageCoffeeController.addListener(  _coffeScrollListener );
+      _pageTextController.addListener( _textScrollListener );
       super.initState();
     }
   
   @override
     void dispose() {
       _pageCoffeeController.removeListener(  _coffeScrollListener );
+      _pageTextController.removeListener(_textScrollListener);
       _pageCoffeeController.dispose();
+      _pageTextController.dispose();
       super.dispose();
     }
 
@@ -68,6 +78,29 @@ class _CoffeeListState extends State<CoffeeList> {
             height: 100.0,
             child: Column(
               children: [
+                Expanded(
+                  child: PageView.builder(
+                    controller: _pageTextController,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: coffees.length,
+                    itemBuilder: (context, index){
+                      final _opacity = (1 - (index - _textPage).abs()).clamp(0.0, 1.0);
+                      return Opacity(
+                        opacity: _opacity,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: _size.width * 0.2 ),
+                          child: Text(
+                            coffees[index].name,
+                            textAlign: TextAlign.center,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.brown[900]),
+                          ),
+                        )
+                      );
+                    }
+                  )
+                ),
                 AnimatedSwitcher(
                   key: Key(coffees[_currentPage.toInt()].name),
                   duration: Duration(milliseconds: 300),
@@ -89,6 +122,11 @@ class _CoffeeListState extends State<CoffeeList> {
               controller: _pageCoffeeController,
               itemCount: coffees.length + 1,
               scrollDirection: Axis.vertical,
+              onPageChanged: (value){
+                if (value < coffees.length){
+                  _pageTextController.animateToPage(value, duration: Duration(milliseconds:300), curve: Curves.easeOut);
+                }
+              },
               itemBuilder: (context, index){
                 if (index == 0) return SizedBox.shrink();
 
