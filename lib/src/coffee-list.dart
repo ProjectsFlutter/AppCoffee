6e6bff97f00b523a81c9a868c22113e0731ff187
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-08-31 10:45:33 -0500

    Lower shadow

--- lib/src/coffee-list.dart
+++ lib/src/coffee-list.dart
@@ -35,6 +35,7 @@ class _CoffeeListState extends State<CoffeeList> {
 
   @override
   Widget build(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -42,6 +43,24 @@ class _CoffeeListState extends State<CoffeeList> {
       ),
       body: Stack(
         children: [
+          Positioned(
+            left: 20,
+            right: 20,
+            bottom: -_size.height * 0.22,
+            height: _size.height * 0.3,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.brown,
+                    blurRadius: 90,
+                    spreadRadius: 45
+                  )
+                ]
+              ),
+            ),
+          ),
           Positioned(
             left: 0,
             top: 0,
@@ -61,17 +80,20 @@ class _CoffeeListState extends State<CoffeeList> {
               final _result = _currentPage - index + 1;
               final _value = -0.4 * _result + 1; 
               final _opacity = _value.clamp(0.0, 1.0);
-              
-              return Transform(
-                alignment: Alignment.bottomCenter,
-                transform: Matrix4.identity()
-                ..setEntry(3,2,0.001)
-                ..translate(0.0, MediaQuery.of(context).size.height / 2.6 * (1 - _value).abs())
-                ..scale(_value),
-                child: Opacity(
-                  opacity: _opacity,
-                  child: Image.asset(coffees[index - 1].image)
-                )
+
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Transform(
+                  alignment: Alignment.bottomCenter,
+                  transform: Matrix4.identity()
+                  ..setEntry(3,2,0.001)
+                  ..translate(0.0, _size.height / 2.6 * (1 - _value).abs())
+                  ..scale(_value),
+                  child: Opacity(
+                    opacity: _opacity,
+                    child: Image.asset(coffees[index - 1].image)
+                  )
+                ),
               );
             }
           )
