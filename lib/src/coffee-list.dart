798a8b6e970c183ced7ecea0ae48ca45235614c9
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-08-31 09:46:59 -0500

    Show images with animation

--- lib/src/coffee-list.dart
+++ lib/src/coffee-list.dart
@@ -7,9 +7,31 @@ class CoffeeList extends StatefulWidget {
 }
 
 class _CoffeeListState extends State<CoffeeList> {
+  
+  double _currentPage = 0.0;
+
   final _pageCoffeeController = PageController(
     viewportFraction: 0.35
   );
+
+  void _coffeScrollListener(){
+    setState(() {
+          _currentPage = _pageCoffeeController.page!;        
+        });
+  }
+
+  @override
+    void initState() {
+      _pageCoffeeController.addListener(  _coffeScrollListener );
+      super.initState();
+    }
+  
+  @override
+    void dispose() {
+      _pageCoffeeController.removeListener(  _coffeScrollListener );
+      _pageCoffeeController.dispose();
+      super.dispose();
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -31,10 +53,26 @@ class _CoffeeListState extends State<CoffeeList> {
           ),
           PageView.builder(
             controller: _pageCoffeeController,
-            itemCount: coffees.length,
+            itemCount: coffees.length + 1,
             scrollDirection: Axis.vertical,
             itemBuilder: (context, index){
-              return Image.asset(coffees[index].image);
+              if (index == 0) return SizedBox.shrink();
+
+              final _result = _currentPage - index + 1;
+              final _value = -0.4 * _result + 1; 
+              final _opacity = _value.clamp(0.0, 1.0);
+              
+              return Transform(
+                alignment: Alignment.bottomCenter,
+                transform: Matrix4.identity()
+                ..setEntry(3,2,0.001)
+                ..translate(0.0, MediaQuery.of(context).size.height / 2.6 * (1 - _value).abs())
+                ..scale(_value),
+                child: Opacity(
+                  opacity: _opacity,
+                  child: Image.asset(coffees[index - 1].image)
+                )
+              );
             }
           )
         ],
