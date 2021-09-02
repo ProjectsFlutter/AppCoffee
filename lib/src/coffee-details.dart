f9657669618478d616db6c637f29f18870e0d023
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-09-02 11:21:06 -0500

    Refactoring coffee list

--- lib/src/coffee-details.dart
+++ lib/src/coffee-details.dart
@@ -21,68 +21,100 @@ class CoffeeDetails extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: _size.width * 0.2),
-            child: Hero(
-              tag:  'text_${coffee.name}',
-              child: Material(
-                color: Colors.white,
-                child: Text(
-                  coffee.name,
-                  maxLines: 2, 
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize:30, 
-                    fontWeight: FontWeight.w600
-                  )
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 30,),
+          _CoffeeName(size: _size, coffee: coffee),
+          SizedBox(height: 30),
           SizedBox(
             height: _size.height * 0.4,
             child: Stack(
               children: [
-                Positioned.fill(
-                  child: Hero(
-                    tag: coffee.name,
-                    child: Image.asset(coffee.image, fit: BoxFit.fitHeight,),
-                  ),
-                ),
-                Positioned(
-                  left: _size.width * 0.05,
-                  bottom: 0,
-                  child: TweenAnimationBuilder<double>(
-                    tween: Tween(begin: 1.0, end: 0.0),
-                    builder: (context, value, child){
-                      return Transform.translate(
-                        offset: Offset(-100 * value , 240 * value),
-                        child: child
-                      );
-                    },
-                    duration: Duration(milliseconds: 500),
-                    child: Text('\$${coffee.price.toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontSize: 50, 
-                            fontWeight: FontWeight.w900,
-                            color: Colors.white,
-                            shadows: [
-                              BoxShadow(
-                                color: Colors.black45,
-                                blurRadius: 10,
-                                spreadRadius: 20
-                              )
-                            ]
-                        )
-                    ),
-                  ),
-                ),
+                _CoffeeImage(coffee: coffee),
+                _CoffeePrice(size: _size, coffee: coffee),
               ],
             ),
           )
         ],
       ), 
+    );
+  }
+}
+
+class _CoffeePrice extends StatelessWidget {
+  final Size _size;
+  final Coffee coffee;
+  const _CoffeePrice({required Size size, required this.coffee}) : _size = size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: _size.width * 0.05,
+      bottom: 0,
+      child: TweenAnimationBuilder<double>(
+        tween: Tween(begin: 1.0, end: 0.0),
+        builder: (context, value, child){
+          return Transform.translate(
+            offset: Offset(-100 * value , 240 * value),
+            child: child
+          );
+        },
+        duration: Duration(milliseconds: 500),
+        child: Text('\$${coffee.price.toStringAsFixed(2)}',
+            style: TextStyle(
+                fontSize: 50, 
+                fontWeight: FontWeight.w900,
+                color: Colors.white,
+                shadows: [
+                  BoxShadow(
+                    color: Colors.black45,
+                    blurRadius: 10,
+                    spreadRadius: 20
+                  )
+                ]
+            )
+        ),
+      ),
+    );
+  }
+}
+
+class _CoffeeImage extends StatelessWidget {
+  final Coffee coffee;
+  const _CoffeeImage({required this.coffee});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Hero(
+        tag: coffee.name,
+        child: Image.asset(coffee.image, fit: BoxFit.fitHeight,),
+      ),
+    );
+  }
+}
+
+class _CoffeeName extends StatelessWidget {
+  final Size _size;
+  final Coffee coffee;
+  const _CoffeeName({required Size size,required this.coffee}) : _size = size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: _size.width * 0.2),
+      child: Hero(
+        tag:  'text_${coffee.name}',
+        child: Material(
+          color: Colors.white,
+          child: Text(
+            coffee.name,
+            maxLines: 2, 
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize:30, 
+              fontWeight: FontWeight.w600
+            )
+          ),
+        ),
+      ),
     );
   }
 }
