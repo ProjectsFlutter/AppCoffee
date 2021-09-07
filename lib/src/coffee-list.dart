343ec3d153eb88285624bd15da50644bf76af389
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-09-07 17:47:24 -0500

    Back button and gradient when starting and going back

--- lib/src/coffee-list.dart
+++ lib/src/coffee-list.dart
@@ -15,6 +15,8 @@ class _CoffeeListState extends State<CoffeeList> {
   double _currentPage = _initialPage;
   double _begin = 1.0;
   double _end = 0.0;
+  double _beginC = 0.0;
+  double _endC = 10.0;
 
   final _pageCoffeeController = PageController(viewportFraction: 0.35, initialPage: _initialPage.toInt());
   final _pageTextController = PageController(initialPage: _initialPage.toInt());
@@ -27,7 +29,7 @@ class _CoffeeListState extends State<CoffeeList> {
   }
 
   void _ValidateToExit() {
-     if (_currentPage == 5 && _close) {
+    if (_currentPage == 5 && _close) {
       Navigator.push(context, MaterialPageRoute(builder: (context) => CoffeeHome()));
     }
   }
@@ -49,79 +51,136 @@ class _CoffeeListState extends State<CoffeeList> {
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          color: Colors.brown,
-          onPressed: () {
-            _close = true;
-            _begin = 0.0;
-            _end = 1.0;
-            _ValidateToExit();
-            _pageCoffeeController.animateToPage(5, duration: Duration(seconds: 1), curve: Curves.linear);
-          },
-        ),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.transparent,
+      //   elevation: 0,
+      //   leading: IconButton(
+      //     icon: Icon(Icons.arrow_back_ios),
+      //     color: Colors.brown,
+      //     onPressed: () {
+      //       _close = true;
+      //       _begin = 0.0;
+      //       _end = 1.0;
+      //       _ValidateToExit();
+      //       _pageCoffeeController.animateToPage(5,
+      //           duration: Duration(seconds: 1), curve: Curves.linear);
+      //     },
+      //   ),
+      // ),
       body: Stack(
         children: [
+          TweenAnimationBuilder<double>(
+              duration: Duration(seconds: 1),
+              tween: Tween(begin: _beginC, end: _endC),
+              builder: (context, value, child) {
+               return Transform.translate(
+                    offset: Offset(0, -100 * value), child: child);
+              },
+              child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Color(0XFFA89276), Color(0XFFFFFF)]
+                    )
+                  )
+              )
+          ),
           _GradientCircle(size: _size),
           Positioned(
+            left: 0,
+            top: 0,
+            right: 0,
+            height: 60,
+            child: Padding(
+              padding: EdgeInsets.only(top: 30, left: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      onPressed: () {
+                        _close = true;
+                        _begin = 0.0;
+                        _end = 2.0;
+
+                        _beginC = 10.0;
+                        _endC = 0.0;
+
+                        _ValidateToExit();
+                        _pageCoffeeController.animateToPage(5, duration: Duration(seconds: 1), curve: Curves.linear);
+                      })
+                ],
+              ),
+            ),
+          ),
+          Positioned(
               left: 0,
-              top: 0,
+              top: 60,
               right: 0,
               height: 100.0,
               child: TweenAnimationBuilder<double>(
                 tween: Tween(begin: _begin, end: _end),
                 builder: (context, value, child) {
-                  return Transform.translate(offset: Offset(0, -100 * value), child: child);
+                  return Transform.translate( offset: Offset(0, -100 * value), child: child);
                 },
                 duration: Duration(milliseconds: 500),
-                child:Column(
+                child: Column(
                   children: [
                     _CoffeeName(pageTextController: _pageTextController, size: _size),
                     _CoffeePrice(currentPage: _currentPage.toInt())
                   ],
                 ),
               )),
-          Transform.scale(
-            scale: 1.7,
-            alignment: Alignment.bottomCenter,
-            child: PageView.builder(
-                controller: _pageCoffeeController,
-                itemCount: coffees.length + 1,
-                scrollDirection: Axis.vertical,
-                onPageChanged: (value) {
-                  if (value < coffees.length) {
-                    _pageTextController.animateToPage(value, duration: Duration(seconds:1), curve: Curves.fastOutSlowIn);
-                  }
-                },
-                itemBuilder: (context, index) {
-                  if (index == 0) return SizedBox.shrink();
-                  final _coffee = coffees[index - 1];
-                  final _result = _currentPage - index + 1;
-                  final _value = -0.4 * _result + 1;
-                  final _opacity = _value.clamp(0.0, 1.0);
-
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(PageRouteBuilder(
-                          transitionDuration: Duration(microseconds: 650),
-                          pageBuilder: (context, animation, _) {
-                            return FadeTransition(
-                              opacity: animation,
-                              child: CoffeeDetails(coffee: _coffee),
-                            );
-                          }));
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 60,
+            bottom: 0,
+            child: Container(
+              color: Colors.transparent,
+              child: Transform.scale(
+                scale: 1.7,
+                alignment: Alignment.bottomCenter,
+                child: PageView.builder(
+                    controller: _pageCoffeeController,
+                    itemCount: coffees.length + 1,
+                    scrollDirection: Axis.vertical,
+                    onPageChanged: (value) {
+                      if (value < coffees.length) {
+                        _pageTextController.animateToPage(value, duration: Duration(seconds: 1), curve: Curves.fastOutSlowIn);
+                      }
                     },
-                    child: _CoffeeImage(
-                        size: _size,
-                        value: _value,
-                        opacity: _opacity,
-                        coffee: _coffee),
-                  );
-                }),
+                    itemBuilder: (context, index) {
+                      if (index == 0) return SizedBox.shrink();
+                      final _coffee = coffees[index - 1];
+                      final _result = _currentPage - index + 1;
+                      final _value = -0.4 * _result + 1;
+                      final _opacity = _value.clamp(0.0, 1.0);
+
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(PageRouteBuilder(
+                              transitionDuration: Duration(microseconds: 650),
+                              pageBuilder: (context, animation, _) {
+                                return FadeTransition(
+                                  opacity: animation,
+                                  child: CoffeeDetails(coffee: _coffee),
+                                );
+                              }
+                          )
+                          );
+                        },
+                        child: _CoffeeImage(
+                            size: _size,
+                            value: _value,
+                            opacity: _opacity,
+                            coffee: _coffee),
+                      );
+                    }
+                ),
+              ),
+            ),
           )
         ],
       ),
@@ -137,10 +196,9 @@ class _CoffeePrice extends StatelessWidget {
   Widget build(BuildContext context) {
     int _positionPage;
     _positionPage = (_currentPage == coffees.length) ? _currentPage - 1 : _currentPage;
-
     return Container(
-        padding: EdgeInsets.symmetric(horizontal: 10.0 ),
-        child: TweenAnimationBuilder<double>(
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      child: TweenAnimationBuilder<double>(
         key: Key(coffees[_positionPage].name),
         duration: Duration(milliseconds: 300),
         tween: Tween(begin: 0.5, end: 1.0),
@@ -159,37 +217,41 @@ class _CoffeePrice extends StatelessWidget {
   }
 }
 
-
 class _CoffeeName extends StatelessWidget {
-  const _CoffeeName({required PageController pageTextController, required Size size}) : _pageTextController = pageTextController,  _size = size;
+  const _CoffeeName({required PageController pageTextController, required Size size}): _pageTextController = pageTextController, _size = size;
   final PageController _pageTextController;
   final Size _size;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: PageView.builder(
-        controller: _pageTextController,
-        physics: NeverScrollableScrollPhysics(),
-        itemCount: coffees.length,
-        itemBuilder: (context, index){
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: _size.width * 0.2 ),
-            child: Hero(
-              tag: 'text_${ coffees[index].name }',
-              child: Material(
-                child: Text(
-                  coffees[index].name,
-                  textAlign: TextAlign.center,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.brown[900]),
+        child: PageView.builder(
+            controller: _pageTextController,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: coffees.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: _size.width * 0.2),
+                child: Hero(
+                  tag: 'text_${coffees[index].name}',
+                  child: Material(
+                    color: Colors.transparent,
+                    child: Text(
+                      coffees[index].name,
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.brown[900]
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          );
-        }
-      )
+              );
+            }
+        )
     );
   }
 }
@@ -206,9 +268,10 @@ class _GradientCircle extends StatelessWidget {
       bottom: -_size.height * 0.22,
       height: _size.height * 0.3,
       child: DecoratedBox(
-        decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
-          BoxShadow(color: Colors.brown, blurRadius: 90, spreadRadius: 45)
-        ]),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle, 
+          boxShadow: [BoxShadow(color: Colors.brown, blurRadius: 90, spreadRadius: 45)]
+        ),
       ),
     );
   }
@@ -220,15 +283,7 @@ class _CoffeeImage extends StatelessWidget {
   final double _opacity;
   final Coffee _coffee;
 
-  const _CoffeeImage({
-    required Size size,
-    required double value,
-    required double opacity,
-    required Coffee coffee,
-  })   : _size = size,
-        _value = value,
-        _opacity = opacity,
-        _coffee = coffee;
+  const _CoffeeImage({required Size size, required double value, required double opacity, required Coffee coffee}): _size = size, _value = value, _opacity = opacity, _coffee = coffee;
 
   @override
   Widget build(BuildContext context) {
@@ -246,7 +301,7 @@ class _CoffeeImage extends StatelessWidget {
                   tag: 'image_${_coffee.name}',
                   child: Image.asset(_coffee.image, fit: BoxFit.fitHeight)
               )
-        )
+          )
       ),
     );
   }
