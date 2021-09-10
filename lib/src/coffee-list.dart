d4c09dfa73d555876f6742b0e1e0c36f284435dd
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-09-09 22:30:26 -0500

    Modification of coffee list animation

--- lib/src/coffee-list.dart
+++ lib/src/coffee-list.dart
@@ -124,7 +124,7 @@ class _CoffeeListState extends State<CoffeeList> {
             child: Container(
               color: Colors.transparent,
               child: Transform.scale(
-                scale: 1.7,
+                scale: 1.8,
                 alignment: Alignment.bottomCenter,
                 child: PageView.builder(
                     controller: _pageCoffeeController,
@@ -139,7 +139,7 @@ class _CoffeeListState extends State<CoffeeList> {
                       if (index == 0) return SizedBox.shrink();
                       final _coffee = coffees[index - 1];
                       final _result = _currentPage - index + 1;
-                      final _value = -0.4 * _result + 1;
+                      final _value = -0.42 * _result + 1;
                       final _opacity = _value.clamp(0.0, 1.0);
 
                       return GestureDetector(
@@ -300,7 +300,7 @@ class _CoffeeImage extends StatelessWidget {
           alignment: Alignment.bottomCenter,
           transform: Matrix4.identity()
             ..setEntry(3, 2, 0.001)
-            ..translate(0.0, _size.height / 2.6 * (1 - _value).abs())
+            ..translate(0.0, _size.height / 3  * (1 - _value).abs())
             ..scale(_value),
           child: Opacity(
               opacity: _opacity,
